65f57253c5fcb5185b9e5dd4f6bd6d83052e9877
Andre Matos <andre_matos13@hotmail.com>
2023-12-18 10:54:49 -0300

    Create migrations

---
+++ database/migrations/2023_12_13_150955_create_addresses_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->id();
+            $table->string('address');
+            $table->integer('zip_code');
+            $table->string('neighborhood');
+            $table->string('city');
+            $table->string('province');
+            $table->string('lat')->default('');
+            $table->string('long')->default('');
+            $table->foreignId('contact_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('addresses');
+    }
+};
