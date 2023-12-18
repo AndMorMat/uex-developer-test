4b6f377c021ad205e6334c027997a74353782138
Andre Matos <andre_matos13@hotmail.com>
2023-12-18 11:12:28 -0300

    Add components

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,21 +1,33 @@
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
-import { Head } from '@inertiajs/react';
+import { AddContact } from "@/Components/Contacts/AddContact";
+import { ContactSearch } from "@/Components/Contacts/ContactSearch";
+import { MapWrapper } from "@/Components/Map";
+import { GlobalProvider } from "@/Contexts/global-context";
+import { MapProvider } from "@/Contexts/map-context";
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import { useEffect } from "react";
 
 export default function Dashboard({ auth }) {
     return (
-        <AuthenticatedLayout
-            user={auth.user}
-            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>}
-        >
-            <Head title="Dashboard" />
-
-            <div className="py-12">
-                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900">You're logged in!</div>
+        <AuthenticatedLayout user={auth.user}>
+            <GlobalProvider>
+                <div className="py-12">
+                    <div className="max-w-screen-2xl mx-auto ">
+                        <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                            <MapProvider>
+                                <div className="p-6 text-gray-900 flex flex-row">
+                                    <div>
+                                        <AddContact />
+                                        <ContactSearch />
+                                    </div>
+                                    <div className="flex-1">
+                                        <MapWrapper />
+                                    </div>
+                                </div>
+                            </MapProvider>
+                        </div>
                     </div>
                 </div>
-            </div>
+            </GlobalProvider>
         </AuthenticatedLayout>
     );
 }
