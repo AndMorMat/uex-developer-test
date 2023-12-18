e0dd16266d12638fa540bf33645dc21a0f80788f
Andre Matos <andre_matos13@hotmail.com>
2023-12-18 10:59:16 -0300

    Create ContactService

--- app/Services/ContactService/ContactService.php
+++ app/Services/ContactService/ContactService.php
@@ -1,10 +1,103 @@
 <?php
 namespace App\Services\ContactService;
 
+use App\Exceptions\ApiException;
+use App\Models\Address;
+use App\Models\Contact;
+use App\Services\GeoLocationService\GeoLocationService;
+use App\Services\GeoLocationService\Model\GeoLocationParams;
+use App\Services\GeoLocationService\Providers\Google\GoogleService;
+use Illuminate\Support\Facades\Auth;
+
 /**
  * Andre Matos <andre_matos13@hotmail.com>
 **/
 class ContactService {
+
+    /**
+     * @var GeoLocationService
+     */
+    private $geolocationService;
+
+    /**
+     * @var User
+     */
+    private $authUser;
+
+    public function __construct() {
+        $this->geolocationService = new GeoLocationService(new GoogleService());
+        $this->authUser = Auth::user();
+    }
+
+    public function getValidation() {
+        return [
+            'name' => 'required|max:255',
+            'cpf' => 'required|string|cpf_ou_cnpj',
+            'address.zip_code' => 'required|integer',
+            'address.neighborhood' => 'required',
+            'address.city' => 'required',
+            'address.province' => 'required'
+        ];
+    }
+
+    public function searchContacts($searchParam = null) {
+
+        if($searchParam) {
+            return Contact::with('address')
+                ->where('user_id', $this->authUser->id)
+                ->where(function ($query) use ($searchParam) {
+                    $query->where('name', 'LIKE', "%$searchParam%")
+                        ->orWhere('cpf', $searchParam);
+                })
+                ->orderBy('id', 'desc')
+                ->get()
+                ->toArray();
+        }
+
+        return Contact::with('address')
+                    ->where('user_id', $this->authUser->id)
+                    ->orderBy('id', 'desc')
+                    ->limit(50)
+                    ->get()
+                    ->toArray();
+    }
+
+    public function addContact($params) {
+        $coordinatesParams = new GeoLocationParams(
+            $params['address']['province'],
+            $params['address']['city'],
+            $params['address']['address'],
+        );
+
+        $coordinates = $this->geolocationService->getCoordinates($coordinatesParams);
+        $params['user_id'] = $this->authUser->id;
+        $contact = Contact::create($params);
+
+        $params['address']['lat'] = $coordinates['lat'];
+        $params['address']['long'] = $coordinates['lng'];
+
+        $address = new Address($params['address']);
+        $contact->address()->save($address);
+    }
+
+    public function updateContact($id, $params) {
+        $contact = Contact::findOrFail($id);
+        if(!$contact) {
+            throw new ApiException('Contato não encontrado');
+        }
+
+        $coordinatesParams = new GeoLocationParams(
+            $params['address']['province'],
+            $params['address']['city'],
+            $params['address']['address'],
+        );
+
+        $coordinates = $this->geolocationService->getCoordinates($coordinatesParams);
+        $contact->update($params);
+        $params['address']['lat'] = $coordinates['lat'];
+        $params['address']['long'] = $coordinates['lng'];
+        $contact->address->update($params['address']);
+    }
 
 
 }
