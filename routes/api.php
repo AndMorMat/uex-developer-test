856ddaa1bc8f9c11ea5c6290d22e2949da9ff7fd
Andre Matos <andre_matos13@hotmail.com>
2023-12-18 11:00:06 -0300

    Create endpoints to cep and contact

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\CepProviders\CepProvidersController;
+use App\Http\Controllers\Contact\ContactController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,7 +15,20 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::controller(CepProvidersController::class)->group(function () {
+    Route::get('/cep/{cepParam}', 'search')->name('cep.search');
+    Route::get('/cep/{province}/{city}/{searchParam}', 'searchStreet')->name('cep.searchstreet');
+});
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/contact', [ContactController::class, 'index'])->name('contact.search');
+    Route::get('/contact-search/{id}', [ContactController::class, 'show'])->name('contact.show');
+    Route::post('/contact-add', [ContactController::class, 'store'])->name('contact.add');
+    Route::put('/contact-update/{id}', [ContactController::class, 'update'])->name('contact.update');
+    Route::delete('/contact-delete/{id}', [ContactController::class, 'destroy'])->name('contact.delete');
+});
+
